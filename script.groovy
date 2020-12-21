43e03984180455805fd9b4aeb0302b170c682559
Ahmad <sohail.yu@gmail.com>
2020-12-20 19:50:38 -0700

    adding script file

---
+++ script.groovy
@@ -0,0 +1,15 @@
+def buildApp() {
+    echo 'building the application...'
+} 
+
+def testApp() {
+    echo 'testing the application...'
+} 
+
+def deployApp() {
+    echo 'deplying the application...'
+    echo "deploying version ${params.VERSION}"
+} 
+
+return this
+
